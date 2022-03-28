4e3d7b7b04d68d2b4c67bf76ab97ae4a65481c01
motsamaiteboho <96079592+motsamaiteboho@users.noreply.github.com>
2022-03-28 13:38:45 +0200

    what i have done

--- js/radio-bill.js
+++ js/radio-bill.js
@@ -7,24 +7,24 @@ const textradioBillAddBtn = document.querySelector(".radioBillAddBtn");
 const callsTotalElement = document.querySelector(".callTotalTwo");
 const smsTotalElement = document.querySelector(".smsTotalTwo");
 const totalCostElement = document.querySelector(".totalTwo");
- 
+
 //create a variable that will keep track of the total bill
 var radiCallsTotal = 0;
 var radiSmsTotal = 0;
 //add an event listener for when the add button is pressed
-function radiBillTotal(){
+function radiBillTotal() {
     // get the value entered in the billType radiobtn checked
-    var billTypeEntered = "";
-    //if(checkedRadioBtn)
-        billTypeEntered = checkedRadioBtn.value;
-    // update the correct total
-    if (billTypeEntered === "call"){
-        radiCallsTotal += 2.75
+    if (checkedRadioBtn) {
+        var billTypeEntered = checkedRadioBtn.value;
+        // update the correct total
+        if (billTypeEntered === "call") {
+            radiCallsTotal += 2.75
+        }
+        else if (billTypeEntered === "sms") {
+            radiSmsTotal += 0.75;
+        }
     }
-    else if (billTypeEntered === "sms"){
-        radiSmsTotal += 0.75;
-    }
-    
+
     //update the totals that is displayed on the screen.
     callsTotalElement.innerHTML = radiCallsTotal.toFixed(2);
     smsTotalElement.innerHTML = radiSmsTotal.toFixed(2);
@@ -35,11 +35,11 @@ function radiBillTotal(){
     totalCostElement.classList.remove("warning");
 
     //color the total based on the criteria
-    if (radiTotalCost >= 50){
+    if (radiTotalCost >= 50) {
         // adding the danger class will make the text red
         totalCostElement.classList.add("danger");
     }
-    else if (radiTotalCost >= 30){
+    else if (radiTotalCost >= 30) {
         totalCostElement.classList.add("warning");
     }
 }
